a1c11bfbc3825183c027394d28ba34c9f184ad76
Abhi0492 <abhi.iyer.abhishek@gmail.com>
2021-04-10 13:16:45 +0530

    Third commit towards my PPM project

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -3,12 +3,21 @@ package io.agileintelligence.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 
 @Service
 public class ProjectService {
 	
 	@Autowired
-	private ProjectRepository projectRepository; 
+	private ProjectRepository projectRepository;
+	
+	public Project saveorUpdateProject(Project project) {
+		
+		
+		//Business Logic
+		
+		return projectRepository.save(project);
+	}
 
 }
