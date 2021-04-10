f9a15bbe9c4b4f5442f9ffc413a47fdc6107b67f
Abhi0492 <abhi.iyer.abhishek@gmail.com>
2021-04-10 13:45:34 +0530

    Fourth commit towards my PPM project

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -2,6 +2,7 @@ package io.agileintelligence.ppmtool.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,6 +10,10 @@ import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
@@ -16,12 +21,21 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank(message = "Project Name is required.")
 	private String projectName;
+	@NotBlank(message = "Project identifier is required.")
+	@Size(min = 4, max = 5, message = "Please use 4 to 5 characters.")
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
+	@NotBlank(message = "Project Description is required.")
 	private String description;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date startDate;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date endDate;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date createdAt;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updatedDate;
 	
 	public Project() {
