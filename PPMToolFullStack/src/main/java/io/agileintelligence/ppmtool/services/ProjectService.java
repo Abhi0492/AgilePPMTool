493f6ca2371d34144c5b5fb25282791f085e8ab9
Abhi0492 <abhi.iyer.abhishek@gmail.com>
2021-04-11 12:56:22 +0530

    Tenth commit towards my PPM project

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -45,5 +45,17 @@ public class ProjectService {
 		
 		return projectRepository.findAll();
 	}
+	
+	public void deleteProjectByIdentifier(String projectId) {
+		
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		
+		if(project == null) {
+			throw new ProjectIdException("Cannot remove Project with ID '"+ projectId +"'. This Project does not exist.");
+		}
+		
+		projectRepository.delete(project);
+		
+	}
 
 }
--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,6 +58,14 @@ public class ProjectController {
 		
 		System.out.println("#################" + projectService.findAllProjects());
 		return projectService.findAllProjects();
+	}
+	
+	@DeleteMapping("/{projectId}")
+	public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+		
+		projectService.deleteProjectByIdentifier(projectId);
+		return new ResponseEntity<String>("Project with ID : '"+ projectId +"' was deleted successfully.", HttpStatus.OK);
+		
 	}
 	
 	
